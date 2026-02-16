b9d4e768466e4062062f65c283c23df5bff50b64
Guilherme <guilherme.dds.dev@gmail.com>
2026-02-16 15:36:23 -0300

    feat: atualiza o layout da página

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 
 function App() {
   const [videoUrl, setVideoUrl] = useState("");
+  const [active, setActive] = useState("video");
 
   const downloadFile = async () => {
     try {
@@ -35,9 +36,39 @@ function App() {
   return (
     <>
       <div className='main'>
-        <h1>Video Downloader</h1>
-        <input type="text" onChange={(e) => setVideoUrl(e.target.value)} />
-        <button onClick={downloadFile}>Download</button>
+        <div className='title'>
+          <h1>VIDEO DOWNLOADER</h1>
+          <p>Projeto desenvolvido apenas para fins de estudo e aprendizado.</p>
+        </div>
+        <div className='download-section'>
+          <div className="section-bar">
+            <div className={`section-video ${active === "video" ? "active" : ""}`} onClick={() => setActive("video")}>Vídeo</div>
+            <div className={`section-audio ${active === "audio" ? "active" : ""}`} onClick={() => setActive("audio")}>Áudio</div>
+          </div>
+          {active === "video" &&
+          <div className='inputs'>
+            <label htmlFor="">URL do vídeo</label>
+            <input type="text" placeholder='https://www.youtube.com/watch?v=' onChange={(e) => setVideoUrl(e.target.value)} />
+            <label htmlFor="">Qualidade do vídeo</label>
+            <select name="" id="" className='select-quality'>
+              <option value="1080p">FULL HD (1080p)</option>
+              <option value="720p">HD (720p)</option>
+            </select>
+          </div>
+          }
+          {active === "audio" &&
+          <div className='inputs'>
+            <label htmlFor="">URL do vídeo</label>
+            <input type="text" placeholder='https://www.youtube.com/watch?v=' onChange={(e) => setVideoUrl(e.target.value)} />
+            <label htmlFor="">Qualidade do áudio</label>
+            <select name="" id="" className='select-quality'>
+              <option value="1080p">MP3 (320kbps)</option>
+              <option value="720p">HD (720p)</option>
+            </select>
+          </div>
+          }
+          <button onClick={downloadFile}>Download</button>
+        </div>
       </div>
     </>
   )
